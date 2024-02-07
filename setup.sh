d81e5b3316997f7e5189216e4c382ac88005d637
roctbb <roctbb@gmail.com>
2024-02-07 15:03:12 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo pip3 install -r requirements.txt
-sudo cp kardi.ini /etc/uwsgi/apps/
+sudo cp kardiru.ini /etc/uwsgi/apps/
 sudo cp agents_kardiru.conf /etc/supervisor/conf.d/
 sudo cp agents_kardiru_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
